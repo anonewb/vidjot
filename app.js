4dd478b7117610e5f6bc58abf6e450f2aa512248
anonewb <anonewbe@gmail.com>
2018-06-19 15:11:20 +0530

    working with template - express-handlebars

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express  = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX');
+  const title = 'Welcome';
+  // res.send('INDEX');
+  res.render('index', {
+    title: title
+  });
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+  // res.send('ABOUT');
+  res.render('about');
 });
+
 
 const port = 5000;
 
