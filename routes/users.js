0959222000b1828949f8ce851e073c034770489b
anonewb <anonewbe@gmail.com>
2018-06-20 19:08:52 +0530

    user registration using bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 
+// Load User modal
+require('../models/User');
+const User = mongoose.model('users');
 
 // ROUTES FOR USERS
 
@@ -19,6 +25,8 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // console.log(req.body);
   // res.send('register');
+
+  // server side validation
   let errors = [];
 
   if(req.body.password != req.body.password2){
@@ -38,7 +46,43 @@ router.post('/register', (req, res) => {
       password2: req.body.password2
     });
   } else {
-    res.send('passed');
+    // check if email is already registered
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user){
+          req.flash('error_msg', 'Email already regsitered');
+          res.redirect('/users/register');
+        } else {
+          // create new user using User model
+          const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+          });
+          // storing pass in db using 'bcrypt'
+          /*
+          1st generating salt of 10 chars,
+          then applying hash to our password using salt,
+          then setting new hashed pass to our pass
+          and finally save in db
+          */
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                });
+            });
+          });
+        }
+      });
   }
 });
 
